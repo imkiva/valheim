10f3c47273203a8b6788b603a438750f2c3982d1
imkiva <imkiva@islovely.icu>
2022-03-11 16:42:59 +0800

    DEBUG: repl parser

---
+++ valheim-core/src/debug/repl.rs
@@ -0,0 +1,50 @@
+use lalrpop_util::lalrpop_mod;
+use crate::isa::typed::Reg;
+use crate::memory::VirtAddr;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Stmt {
+  Eval(Expr),
+  AddWatch(Expr),
+  RemoveWatch(u64),
+  AddBreakpoint(VirtAddr),
+  RemoveBreakpoint(VirtAddr),
+  ShowRelevantAsm,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Expr {
+  Literal(u64),
+  Register(Reg),
+  Deref(Box<Expr>),
+  Assign(Box<Expr>, Box<Expr>),
+  Add(Box<Expr>, Box<Expr>),
+  Sub(Box<Expr>, Box<Expr>),
+  Mul(Box<Expr>, Box<Expr>),
+  Div(Box<Expr>, Box<Expr>),
+}
+
+#[derive(Debug, Clone)]
+pub struct ReplParseError {
+  pub kind: ReplParseErrorKind,
+  pub location: usize,
+}
+
+#[derive(Debug, Clone)]
+pub enum ReplParseErrorKind {
+  LiteralTooLarge,
+}
+
+lalrpop_mod!(pub grammar);
+
+pub struct ReplParser {}
+
+impl ReplParser {
+  pub fn parse(src: &str) -> Option<Stmt> {
+    match grammar::StmtParser::new().parse(src) {
+      Ok(stmt) => Some(stmt),
+      Err(_) => None,
+    }
+  }
+}
+
