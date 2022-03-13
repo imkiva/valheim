22e05c337ea2149c0c9bb518ce272e68fb6c85a9
imkiva <imkiva@islovely.icu>
2022-03-13 15:10:29 +0800

    TESTS: add test kernel

---
+++ tests/kernel0/kernel.c
@@ -0,0 +1,11 @@
+// tell valheim that we are done
+__attribute__((noreturn)) extern void kernel_halt();
+
+// the kernel entrace function called by boot.S
+void kernel_main(void) {
+  int sum = 0;
+  for (int i = 0; i < 100; ++i) {
+    sum += i;
+  }
+  kernel_halt();
+}
