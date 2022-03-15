7dcd6b5c6730889f5be59d20be6a019217a96632
imkiva <imkiva@islovely.icu>
2022-03-15 15:51:36 +0800

    TESTS: use uart for output

--- tests/kernel0/kernel.c
+++ tests/kernel0/kernel.c
@@ -1,14 +1,17 @@
+#include "uart.h"
+
 // tell valheim that we are done
 __attribute__((noreturn)) extern void kernel_halt();
 
 // the kernel entrace function called by boot.S
 void kernel_main(void) {
+  uart_init();
+  uart_puts("Hello, kernel!\n");
+
   int sum = 0;
   for (int i = 0; i <= 100; ++i) {
     sum += i;
   }
 
-  volatile int *ptr = (int *) 0xb8000;
-  *ptr = sum;
   kernel_halt();
 }
---
+++ tests/kernel0/uart.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void uart_init(void);
+void uart_puts(char *s);
+int uart_putc(char ch);
