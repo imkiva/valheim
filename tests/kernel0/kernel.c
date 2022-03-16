c3d47dee222a00b08cec152be08015c107725d38
imkiva <imkiva@islovely.icu>
2022-03-16 14:44:32 +0800

    TESTS: added gdbinit

--- tests/kernel0/kernel.c
+++ tests/kernel0/kernel.c
@@ -3,9 +3,9 @@
 // tell valheim that we are done
 __attribute__((noreturn)) extern void kernel_halt();
 
-// the kernel entrace function called by boot.S
+// the kernel entrance function called by boot.S
 void kernel_main(void) {
   uart_init();
-  uart_puts("A");
+  uart_puts("hello kernel\n");
   kernel_halt();
 }
