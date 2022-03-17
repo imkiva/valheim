aa45b07b3412cfc1f32dcd2204191704587e0584
imkiva <imkiva@islovely.icu>
2022-03-17 20:16:39 +0800

    PROFILE: fix argument

--- profiler/profile.sh
+++ profiler/profile.sh
@@ -3,7 +3,7 @@
 cargo build --release
 
 sudo dtrace -c \
-  '../target/release/valheim --kernel tests/test-kernel.bin --bios tests/rustsbi-qemu.bin' \
+  '../target/release/valheim --kernel ../tests/test-kernel.bin --bios ../tests/rustsbi-qemu.bin' \
   -o out.stacks \
   -n 'profile-997/execname == "valheim"/{@[ustack(500)] = count();}'
 
