88e5c49c101a810d8cc8731cff5e302b70c01e1b
imkiva <imkiva@islovely.icu>
2022-04-02 23:53:38 +0800

    TEST: add rv64uf-p tests

--- valheim-testing/sync-tests.sh
+++ valheim-testing/sync-tests.sh
@@ -6,9 +6,11 @@ set -o pipefail
 function find_glob() {
   local pattern="$1"
   local dir="$2"
+  local disabled="$3"
   find "$dir" -type f -name "$pattern" -exec basename {} \; \
     | grep -v "\\.dump$" \
     | grep -v "\\.bin$"  \
+    | grep -v -x -F -f "$disabled" \
     | sort               \
     | uniq
 }
@@ -16,6 +18,9 @@ function find_glob() {
 SELF="$(dirname $0)"
 
 ENABLED_TESTS_FILE="$SELF/enabled-tests.txt"
+DISABLED_TESTS_FILE="$SELF/disabled-tests.txt"
 ISA_TEST_DIR="$SELF/target/share/riscv-tests/isa"
 
-find_glob 'rv64ui-p-*' "$ISA_TEST_DIR" > "$ENABLED_TESTS_FILE"
+> "$ENABLED_TESTS_FILE"
+find_glob 'rv64ui-p-*' "$ISA_TEST_DIR" "$DISABLED_TESTS_FILE" >> "$ENABLED_TESTS_FILE"
+find_glob 'rv64uf-p-*' "$ISA_TEST_DIR" "$DISABLED_TESTS_FILE" >> "$ENABLED_TESTS_FILE"
